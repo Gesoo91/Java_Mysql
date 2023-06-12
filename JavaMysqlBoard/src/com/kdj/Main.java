536e15b34898164579eb1cd5d5d40df2f1019cd6
Gesoo91 <132542471+Gesoo91@users.noreply.github.com>
2023-06-12 17:37:28 +0900

    Add files via upload

---
+++ JavaMysqlBoard/src/com/kdj/Main.java
@@ -0,0 +1,17 @@
+package com.kdj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@SuppressWarnings("unused")
+public class Main {
+	public static void main(String[] args) {
+		ProcBoard procBoard=new ProcBoard();
+		procBoard.run();
+	}
+	
+	
+
+}
